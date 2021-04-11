926757437955fa2b95727c6c318c1dfc99def44f
Ivana Quintero <ivananakaret@gmail.com>
2021-04-11 19:56:13 -0300

    Mi primer commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <ul>
+            <li>
+                <a href=" ">Lista 1</a>
+            </li>
+            <li>
+                <a href=" ">Lista 2</a>
+            </li>
+            <li>
+                <a href=" ">Lista 3</a>
+            </li>
+            <li>
+                <a href=" ">Lista 4</a>
+            </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
