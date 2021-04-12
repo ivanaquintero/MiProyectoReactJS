73da36f13b7fd06780ab54300d7ed05cffea7fd5
Ivana Quintero <ivananakaret@gmail.com>
2021-04-11 22:12:02 -0300

    Mi segundo commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
 
@@ -5,22 +6,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      <ul>
-            <li>
-                <a href=" ">Lista 1</a>
-            </li>
-            <li>
-                <a href=" ">Lista 2</a>
-            </li>
-            <li>
-                <a href=" ">Lista 3</a>
-            </li>
-            <li>
-                <a href=" ">Lista 4</a>
-            </li>
-      </ul>
     </div>
   );
 }
+
 
 export default App;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Divider from '@material-ui/core/Divider';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,17 +21,20 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DenseAppBar() {
   const classes = useStyles();
-
+ 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+            Global Deportes
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
+          <MenuList>
+                    <MenuItem>Lista 1</MenuItem>
+                    <MenuItem>Lista 2</MenuItem>
+                    <MenuItem>Lista 3</MenuItem>
+          </MenuList>    
           </IconButton>
-          
-          <Typography variant="h6" color="inherit">
-            Global Deportes - Venta de Productos para Entrenar
           </Typography>
         </Toolbar>
       </AppBar>
